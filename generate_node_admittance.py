b91bda931fb91e42b3f411938fa35a33ff4adac9
leo <cnmwozhendebuzhidao@gmail.com>
2015-01-18 16:13:05 +0800

    添加csv DictReader ,原始数据增加备注

--- generate_node_admittance.py
+++ generate_node_admittance.py
@@ -4,29 +4,26 @@ import csv
 from numpy import *
 #第一次打开文件,统计最大的节点数
 with open('data.csv',newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
+    spamreader = csv.DictReader(csvfile)
     arraySize=4
     for row in spamreader:
-        a=list(row)
-        print(int(a[1]))
-        if int(a[0])>arraySize:
-            arraySize=int(a[0])
-        if int(a[1])>arraySize:
-            arraySize=int(a[1])
+        if int(row['toport'])>arraySize:
+            arraySize=int(row['toport'])
+        if int(row['fromport'])>arraySize:
+            arraySize=int(row['fromport'])
     print('最大值为',arraySize)
 #第二次打开文件,形成导纳矩阵
 with open('data.csv',newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
+    spamreader = csv.DictReader(csvfile)
     Ybus=zeros((arraySize,arraySize),complex)
     for row in spamreader:
         #print('in!')
-        a=list(row)
         #print(int(a[1]))
-        fromport=int(a[0])-1
+        fromport=int(row['fromport'])-1
         #print('来源',fromport)
-        toport=int(a[1])-1
-        Z=complex(float(a[2]),float(a[3]))
-        BOrK=float(a[4])
+        toport=int(row['toport'])-1
+        Z=complex(float(row['R']),float(row['X']))
+        BOrK=float(row['BOrK'])
         Y=1/Z
         if BOrK>0.85 :
             #如果大于0.85,认为是变压器,做相关变换
@@ -45,6 +42,7 @@ with open('Ybus.csv','w',newline='') as YbusFile:
     writer.writerows(Ybus)
 print(Ybus)
 #print a
+
 '''
 file_object = open('data.csv')
 try:
